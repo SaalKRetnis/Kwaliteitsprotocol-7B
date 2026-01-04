e9713f93788ed59ede98d3e073e9bfcd1734e031
evanderzwan <evanderzwan@gmail.com>
2026-01-04 19:49:45 +0100

    update

--- Kwaliteitsprotocol 7B/Services/HintsService.cs
+++ Kwaliteitsprotocol 7B/Services/HintsService.cs
@@ -13,7 +13,7 @@ public sealed class HintsService
         new("Tseard heeft ooit op een zonnige middag een gin-tonic besteld. Hij drinkt geen alcohol...", "an2-speech", "img/an2.png", "Het tweede cijfer is een <strong>1</strong>."),
         new("Koudummer Rummikub is raar en volstrekt onnodig.", "an3-speech", "img/an3.png", "Alle cijfers zijn <strong>numeriek</strong>."),
         new("Tseard heeft onbedoeld bij ons het Koudummer kwartiertje geïntroduceerd.", "an4-speech", "img/an4.png", "Het eerste cijfer is <strong>vandaag</strong>."),
-        new("Wie kan dit moment nou vergeten...", "an5-speech", "img/an5.png", "De laatste twee cijfers slaan op het <strong>jaartal</strong>."), // todo auto foto
+        new("Wie kan dit moment nou vergeten...", "an5-speech", "img/an5.png", "De laatste twee cijfers slaan op het <strong>jaartal</strong>."),
         new("In Engeland vergat Tseard even dat ze daar links rijden...", "an6-speech", "img/an6.png", "Alleen het derde cijfer is een <strong>priemgetal</strong>."),
     ];
 
